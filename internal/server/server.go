873ca61fe02364afa27df5ca909d5f43d0dfaea1
honeyjain0097 <honeyjain0097@gmail.com>
2023-03-13 19:23:22 +0530

    feat: main fiber app added

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,32 +2,28 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/tetrex/coffeeshop-crud-golang/internal/routes"
-	"golang.org/x/sync/errgroup"
 )
 
 func FiberApp() *fiber.App {
 	app := fiber.New(fiber.Config{
 		Prefork: false,
 	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // comma string format e.g. "localhost, nikschaefer.tech"
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+	}))
+	app.Use(logger.New())
+
 	// ---------------
 	// ROUTER
+	routes.Initilize(app)
 	// ---------------
 	//
 	// ---------------
 
-	var eg errgroup.Group
-	eg.Go(func() error {
-		return app.Listen(":8080")
-	})
-
 	return app
-}
-
-func RootApi(app *fiber.App) {
-	rootRouter := routes.FiberRouter(app)
-
-	rootRouter.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("respond with a resource")
-	})
 }
