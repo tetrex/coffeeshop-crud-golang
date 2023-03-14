a9a94f30098952efe231370f76b35c7f20c39858
tetrex <rawat1042@gmail.com>
2023-03-14 17:45:42 +0530

    feat: empoloyees.service, added Create

---
+++ internal/employees/employeesService/employees.service.go
@@ -0,0 +1,33 @@
+package employeesservice
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	employeesschema "github.com/tetrex/coffeeshop-crud-golang/internal/employees/employeesSchema"
+	"github.com/tetrex/coffeeshop-crud-golang/internal/employees/employeesService/dto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func Create(dto *dto.Employees) (bool, error) {
+
+	employeeObj := employeesschema.EmployeesSchema{
+		Role:      dto.Role,
+		Name:      dto.Name,
+		Email:     dto.Email,
+		Password:  dto.Password,
+		Deleted:   false,
+		CreatedAt: primitive.Timestamp{T: uint32(time.Now().Unix())},
+		UpdatedAt: primitive.Timestamp{T: uint32(time.Now().Unix())},
+	}
+
+	result, err := employeesschema.EmployeeCollection.InsertOne(context.TODO(), employeeObj)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	fmt.Printf("inserted :: %s", result.InsertedID)
+
+	return true, nil
+}
