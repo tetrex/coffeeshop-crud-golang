552dcfb7d1c5a2f271354234c0b5e310b16a7179
tetrex <rawat1042@gmail.com>
2023-03-14 13:26:49 +0530

    feat: internal/employees/schema added

---
+++ internal/employees/employeesSchema/employees.schema.go
@@ -0,0 +1,28 @@
+package employeesschema
+
+import (
+	"github.com/tetrex/coffeeshop-crud-golang/internal/db"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ----------------
+// collection
+var EmployeeCollection = db.MongoDataBase.Collection("employees")
+
+// ----------------
+
+type EmployeeRoles string
+
+const (
+	ADMIN_ROLE   EmployeeRoles = "ADMIN"
+	MANAGER_ROLE EmployeeRoles = "MANAGER"
+	BARISTA_ROLE EmployeeRoles = "BARISTA"
+)
+
+type Employees struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Role     EmployeeRoles      `bson:"role,omitempty"`
+	Name     string             `bson:"name,omitempty"`
+	Email    string             `bson:"email,omitempty"`
+	Password string             `bson:"password,omitempty"`
+}
