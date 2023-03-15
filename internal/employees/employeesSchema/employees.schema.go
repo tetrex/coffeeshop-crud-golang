a2f26264594bf95245c87de47a01dcaad0307a72
tetrex <rawat1042@gmail.com>
2023-03-16 01:46:24 +0530

    fix:employee/schema,updated,role type

--- internal/employees/employeesSchema/employees.schema.go
+++ internal/employees/employeesSchema/employees.schema.go
@@ -30,7 +30,7 @@ const (
 
 type Employees struct {
 	ID        primitive.ObjectID  `bson:"_id,omitempty"`
-	Role      EmployeeRoles       `bson:"role,omitempty"`
+	Role      []string            `bson:"role,omitempty"`
 	Name      string              `bson:"name,omitempty"`
 	Email     string              `bson:"email,omitempty"`
 	Password  string              `bson:"password,omitempty"`
